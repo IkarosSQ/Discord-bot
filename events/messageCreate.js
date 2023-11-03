010b1d8a2ad1ed3d46f4e4dcfe6f29e7dab56d37
IkarosSQ <federleyj@gmail.com>
2023-11-03 13:14:33 +0100

    asdsa

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,10 +4,6 @@ module.exports = {
     name: 'messageCreate',
     once: false,
     async execute(message) {
-        if (message.author.bot) return; // Stops bot from triggering loop if done correctly
-        
-        
-        // do your stuff
-        await message.reply("fixed")
+        if (message.author.bot) return; // Stops bot from triggering loop if done correctly)
     }
 };
