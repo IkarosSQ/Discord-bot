e5f9f21a921367ff8c5934f5b42e8658c7801267
IkarosSQ <federleyj@gmail.com>
2023-11-06 09:37:40 +0100

    aaa

--- commands/catme.js
+++ commands/catme.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
+const {readLink} = require("fs");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('catme')
@@ -6,7 +7,7 @@ module.exports = {
 
     async execute(interaction) {
         
-        await interaction.reply({files:[fs.link.readLink("https://i0.wp.com/dianaurban.com/wp-content/uploads/2017/07/01-cat-stretching-feet.gif?resize=500%2C399&ssl=1")]});
+        await interaction.reply({files:[fs.readLink("https://i0.wp.com/dianaurban.com/wp-content/uploads/2017/07/01-cat-stretching-feet.gif?resize=500%2C399&ssl=1")]});
     },
 };
 //dette er en command som i teorien skal sende en gif når man skriver "/catme"
