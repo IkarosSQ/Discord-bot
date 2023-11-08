11cde4163a349063021a9806dd047b53c161a0ff
IkarosSQ <federleyj@gmail.com>
2023-11-08 11:41:55 +0100

    sa

--- commands/catme.js
+++ commands/catme.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-const links = [L1, L2, L3, L4, L5, L6, L7, L8, L9, L0];
-let L0 = "https://images.hive.blog/p/2ufhwNgM3qHKGxq8wdcWAiFpcm29FF1vQ7VZHkTuxDL2jbDCRyUWSPzuZcaFUA3fmHzubFgSq?format=match&mode=fit"
+
 let L1 = "https://i0.wp.com/dianaurban.com/wp-content/uploads/2017/07/01-cat-stretching-feet.gif?resize=500%2C399&ssl=1"
 let L2 = "https://media.tenor.com/7JbbkdTGA38AAAAd/cute-cat.gif"
 let L3 = "https://www.cutecatgifs.com/wp-content/uploads/2021/02/wow.gif"
@@ -10,7 +9,8 @@ let L6 = "https://media.tenor.com/QFLzwvAd-LMAAAAM/cat-sope.gif"
 let L7 = "https://media.tenor.com/bhz9WIjKpeQAAAAd/funny-cat.gif"
 let L8 = "https://media3.giphy.com/media/12d71hRlD9T2Cc/200w.gif?cid=6c09b952x78kqbljr8p4qxmyio2ir9ua05d4qjohct8ctrg0&ep=v1_gifs_search&rid=200w.gif&ct=g"
 let L9 = "https://media1.giphy.com/media/v6aOjy0Qo1fIA/200w.gif?cid=6c09b9526sidj4hc6t7lk6wgv1mqhnkovaby2q15augl63jg&ep=v1_gifs_search&rid=200w.gif&ct=g"
-
+let L0 = "https://images.hive.blog/p/2ufhwNgM3qHKGxq8wdcWAiFpcm29FF1vQ7VZHkTuxDL2jbDCRyUWSPzuZcaFUA3fmHzubFgSq?format=match&mode=fit"
+const links = [L1, L2, L3, L4, L5, L6, L7, L8, L9, L0];
 
 module.exports = {
     data: new SlashCommandBuilder()
