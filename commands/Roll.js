640441a1ed9e131c43c46fac16b8b353bb1e77e6
IkarosSQ <federleyj@gmail.com>
2023-11-15 13:30:00 +0100

    esa

--- commands/Roll.js
+++ commands/Roll.js
@@ -3,17 +3,18 @@ const { SlashCommandBuilder } = require("discord.js");
 
 const onetoonehundred = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    return Mathfloor(Math.random() * (max - min) + min);
   }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('Roll')
-        .setDescription('roll from 10 to 100'),
+        .setDescription('roll from 10 to 20'),
 
         
     async execute(interaction) {
         console.log("test")
-        await interaction.reply("congrats! you rolled a:" + getRandomArbitrary);
+        const rolledNumber = getRandomArbitrary
+        await interaction.reply(`congrats! you rolled a: ${rolledNumber}` );
     },
 };
